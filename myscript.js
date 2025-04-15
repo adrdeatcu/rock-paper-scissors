c8def57284350c2be4aa3ef03a6e32c9bd370351
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2025-04-16 01:25:44 +0300

    feat creating the rounds logic using if/else if for every possible case

--- myscript.js
+++ myscript.js
@@ -10,21 +10,50 @@ function getComputerChoice()
     let odd = Math.random();
     let weapon = undefined;
     if(odd<=0.33)
-    {
-        weapon="Rock";
-    }else if(odd>0.33 && odd<=0.66)
-    {
-        weapon="Paper";
-    }else{
-        weapon="Scissors";
-    }
-    
-    return weapon;
+        {
+            weapon="Rock";
+        }else if(odd>0.33 && odd<=0.66)
+            {
+                weapon="Paper";
+            }else{
+                weapon="Scissors";
+            }
+            return weapon;
 }
-
+        
 function playRound(humanChoice, computerChoice)
 {
-    String(humanChoice).
+    let caseInsensitiveHumanChoice = String(humanChoice).charAt(0).toUpperCase() + String(humanChoice).slice(1);
+    if(caseInsensitiveHumanChoice==computerChoice)
+    {
+        console.log(`It's a tie! You both choosed the ${computerChoice}`);
+        humanScore++;
+        computerScore++;
+    }else if(caseInsensitiveHumanChoice=="Rock" && computerChoice=="Paper")
+    {
+        console.log("You lost! Rock is defeated by the Paper! :(");
+        computerScore++;
+    }else if(caseInsensitiveHumanChoice=="Rock" && computerChoice=="Scissors")
+    {
+        console.log("You won! Rock is destroying the Scissors :)");
+        humanScore++;
+    }else if(caseInsensitiveHumanChoice=="Paper" && computerChoice=="Rock")
+    {
+        console.log("You won! Paper is destroying the Rock :)");
+        humanScore++;
+    }else if(caseInsensitiveHumanChoice=="Paper" && computerChoice=="Scissors")
+    {
+        console.log("You lost! Scissors are destroying the Paper :(");
+        computerScore++;
+    }else if(caseInsensitiveHumanChoice=="Scissors" && computerChoice=="Paper")
+    {
+        console.log("You won! Scissors are destroying the Paper :)");
+        humanScore++;
+    }else if(caseInsensitiveHumanChoice=="Scissors" && computerChoice=="Rock")
+    {
+        console.log("You lost! Scissors are destroyed by the Rock :(");
+        computerScore++;
+    }
 }
 
 
@@ -33,5 +62,7 @@ let computerScore = 0;
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+
 
 playRound(humanSelection,computerSelection);
